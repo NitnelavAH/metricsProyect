ce5f0f465ab0602c79c10e8ffff3d03b4e640fe2
Valentin Abundo <valentin.abundo@bambu-mobile.com>
2021-09-20 23:00:55 -0500

    scanner class and add activities

--- app/src/main/java/com/nitnelav/metricas/halstead/Metrics.java
+++ app/src/main/java/com/nitnelav/metricas/halstead/Metrics.java
@@ -1,16 +1,14 @@
 package com.nitnelav.metricas.halstead;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-
-
-public class MainActivity extends AppCompatActivity {
+public class Metrics extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_metrics);
     }
-
 }
