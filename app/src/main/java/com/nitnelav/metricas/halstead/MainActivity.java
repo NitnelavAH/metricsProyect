99ff7510725fa4062d8c6c01d265cf8405adc9c9
Valentin Abundo <valentin.abundo@bambu-mobile.com>
2021-09-29 01:04:34 -0500

    add new activities

--- app/src/main/java/com/nitnelav/metricas/halstead/MainActivity.java
+++ app/src/main/java/com/nitnelav/metricas/halstead/MainActivity.java
@@ -2,189 +2,37 @@ package com.nitnelav.metricas.halstead;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private final int CHOOSE_FILE_FROM_DEVICE = 1001;
-    private final String TAG = "MainActivity";
-
-    private Button chooseFile_btn;
-    private TextView path_file;
-    private EditText code_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        chooseFile_btn = findViewById(R.id.choose_file_btn);
-        path_file = findViewById(R.id.path_file);
-        code_view = (EditText) findViewById(R.id.code_view);
-
-
-        chooseFile_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                callChooseFileFromDevice();
-            }
-        });
     }
 
-    private void callChooseFileFromDevice() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("text/plain");
-        startActivityForResult(intent, CHOOSE_FILE_FROM_DEVICE);
+    public void goToMetrics(View v){
+        Intent metrics = new Intent(this, New_Metrics_Activity.class);
+        startActivity(metrics);
+    }
+    
+    public void goToDb(View v){
+        Intent dbMetrics = new Intent(this, Metrics_Activity.class);
+        startActivity(dbMetrics);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-        if (requestCode == CHOOSE_FILE_FROM_DEVICE && resultCode == Activity.RESULT_OK) {
-            if(resultData != null) {
-                Log.d(TAG, "onActivityResult: "+ resultData.getData().getPath());
-                Log.d(TAG, "onActivityResult: "+ resultData.getDataString());
-                path_file.setText("File Path:" + resultData.getData() );
-                try {
-                    String code = readTextFromUri(resultData.getData());
-                    String resuslt = getTokens(resultData.getData());
-                    Log.d(TAG, "onActivityResult: "+ code);
-                    code_view.setText(code + "\n" + resuslt);
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
+    public void goToHelp(View v){
+        Intent help = new Intent(this, Help_Activity.class);
+        startActivity(help);
     }
 
-    private String readTextFromUri(Uri uri) throws IOException {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        try (InputStream inputStream = getContentResolver().openInputStream(uri);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)))
-
-        ) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if(line.length()>0 && !line.startsWith("#")){
-                    stringBuilder.append(line + "\n");
-                    Log.d(TAG, "onActivityResult: "+ line);
-                }
-
-            }
-        }
-        return stringBuilder.toString();
-    }
-
-    private String getTokens(Uri uri) {
-        int operadores = 0;
-        int operandos = 0;
-        int errors = 0;
-        String resultado = "\n ----RESULTADOS---- \n";
-        try (InputStream inputStream = getContentResolver().openInputStream(uri);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)))
-        ) {
-            LexerAnalizer lexer = new LexerAnalizer(reader, true);
-            PyTokens token;
-            ArrayList<String> myTokens = new ArrayList<String>();
-            while ((token = lexer.yylex()) != null) {
-                Log.d(TAG, "onActivityResult: " + lexer.yylex());
-                Log.d(TAG, "onActivityResult: " + lexer.yytext());
-                myTokens.add(lexer.yytext());
-                switch (token){
-                    case KEYWORD:
-                    case OPERATOR:
-                        operadores +=1;
-                        resultado += lexer.yytext() + " --> Operador \n";
-                        break;
-                    case LITERAL:
-                    case IDENTIFIER:
-                        operandos +=1;
-                        resultado += lexer.yytext() + " --> Operando \n";
-                        break;
-                    default:
-                }
-            }
-            String result = lexer.getReviewString();
-            //resultado += result;
-            resultado += "\nNo. Operadores: "+ operadores+"\nNo.Operandos: "+operandos;
-            lexer.printReview();
-
-
-            Map<String, Integer> tokenCount = new HashMap<>();
-
-            for (String myToken : myTokens) {
-                String[] tokens = myToken.split(" ");
-                for (String item : tokens) {
-                    Integer count = tokenCount.get(item);
-                    if (count == null)
-                        count = 0; // first time this token was found
-
-                    tokenCount.put(item, count+1);
-                }
-            }
-
-            for (Map.Entry entry : tokenCount.entrySet())
-            {
-                Log.d(TAG, "onActivityResult: "+ "key: " + entry.getKey() + "; value: " + entry.getValue());
-            }
-
-            /*while(true){
-                Tokens tokens = lexer.yylex();
-                Log.d(TAG, "onActivityResult: " + lexer.lexeme);
-                if (tokens == null) {
-                    resultado += "No. Operadores: " + operadores + "\n" + "No. Operandos: " + operandos + "\n" + "No. Errores: " + errors + "\n";
-                    break;
-                }
-                switch (tokens) {
-                    case Operador:
-                        operadores += 1;
-                        resultado += lexer.lexeme + ":Es un " + tokens + "\n";
-                        break;
-                    case Operando:
-                        operandos += 1;
-                        resultado += lexer.lexeme + ":Es un " + tokens + "\n";
-                        break;
-                    case ERROR:
-                        errors += 1;
-                        resultado += lexer.lexeme + ":Es un " + tokens + "\n";
-                    default:
-                        resultado += "Token: " + tokens + '\n';
-                        break;
-                }
-            }*/
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return resultado;
+    public void goToAbout(View v){
+        Intent about = new Intent(this, About_Activity.class);
+        startActivity(about);
     }
 }
 
--- app/src/main/java/com/nitnelav/metricas/halstead/Metrics.java
+++
@@ -1,6 +0,0 @@
-package com.nitnelav.metricas.halstead;
-
-public class Metrics {
-
-
-}
